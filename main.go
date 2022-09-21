052b43c23a03f646f6234cad43903b6fb027367d
4thabang <puresneakers1@gmail.com>
2022-09-21 08:31:44 +0100

    test: testing more

--- main.go
+++ main.go
@@ -4,16 +4,37 @@ import (
 	"fmt"
 )
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	chinese            = "Chinese"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	chineseHelloPrefix = "NiHao, "
+)
 
 func main() {
-	hello := Hello("")
+	hello := Hello("", "")
 	fmt.Println(hello)
 }
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case chinese:
+		prefix = chineseHelloPrefix
+	}
+	return
 }
