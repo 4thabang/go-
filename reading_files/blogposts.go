7604fbc20dbd4392678f10018421e737318e0559
4thabang <puresneakers1@gmail.com>
2023-02-26 21:17:25 +0000

    test: reading files test

---
+++ reading_files/blogposts.go
@@ -0,0 +1,50 @@
+package blogposts
+
+import (
+	"io"
+	"io/fs"
+)
+
+type Post struct {
+	Title string
+	/*
+	 *	Description, Body string
+	 *  Tags                     []string
+	 */
+}
+
+func NewPostFromFS(fileSystem fs.FS) ([]Post, error) {
+	var posts []Post
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if len(dir) == 0 {
+		return nil, err
+	}
+	for _, f := range dir {
+		p, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
+}
