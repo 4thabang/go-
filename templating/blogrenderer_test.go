04af2795a0a931c503cda36b999521bf94f23161
4thabang <puresneakers1@gmail.com>
2023-02-28 14:00:47 +0000

    test: templating test

--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -3,28 +3,41 @@ package blogrenderer_test
 import (
 	"bytes"
 	blogrenderer "go-tests/templating"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
 )
 
-func TestRender(t *testing.T) {
-	var (
-		aPost = blogrenderer.Post{
-			Title:       "hello world",
-			Body:        "This is a post",
-			Description: "This is a description",
-			Tags:        []string{"go", "tdd"},
-		}
-	)
+var (
+	aPost = blogrenderer.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+)
 
+func TestRender(t *testing.T) {
 	t.Run("converts single post to HTML", func(t *testing.T) {
 		var buf bytes.Buffer
+		templ, err := blogrenderer.NewPostRender()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := templ.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	templ, _ := blogrenderer.NewPostRender()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		templ.Render(io.Discard, aPost)
+	}
 }
