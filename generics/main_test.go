49700b2a382d21cca8e6ea8d3eb5e9f7e6ed0211
4thabang <puresneakers1@gmail.com>
2023-03-02 13:06:01 +0000

    test: generics test

--- generics/main_test.go
+++ generics/main_test.go
@@ -4,4 +4,28 @@ import (
 	"testing"
 )
 
-func TestAssertFunctions(t *testing.T) {}
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting int", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting string", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "world")
+	})
+}
+
+func AssertEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got: %+v, want: %+v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("%+v should not be equal to: %+v", want, got)
+	}
+}
