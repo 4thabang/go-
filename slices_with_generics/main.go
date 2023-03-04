edf36f5a3c929b1779ef2b4bbf0671a946f0550b
4thabang <puresneakers1@gmail.com>
2023-03-04 14:34:41 +0000

    test: array / slice generics test

--- slices_with_generics/main.go
+++ slices_with_generics/main.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
+type Typer interface {
+	string | int
+}
+
 func main() {
+	value := SumFunction([]int{1, 2, 3, 4})
+	fmt.Println(value)
+
 	slices := [][]int{{1, 2}, {2, 3}}
 	result := SumAll(slices...)
 	fmt.Println(result)
 }
 
 func SumFunction(i []int) int {
-	var result int
-	for _, num := range i {
-		result += num
-	}
-	return result
+	sum := reduce(i, 0, reducer[int])
+	return sum
 }
 
 func SumAll(numbers ...[]int) []int {
@@ -24,4 +28,16 @@ func SumAll(numbers ...[]int) []int {
 		}
 	}
 	return newSlice
+}
+
+func reduce[T Typer](arr []T, initValue T, fn func(first, second T) T) T {
+	for _, value := range arr {
+		initValue = fn(initValue, value)
+	}
+	return initValue
+}
+
+func reducer[T Typer](initValue T, value T) T {
+	initValue = initValue + value
+	return initValue
 }
