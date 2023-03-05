2d4b4fb25dc20832285e05851df4f60ab0eb77c0
4thabang <puresneakers1@gmail.com>
2023-03-05 15:16:34 +0000

    test: demo test

---
+++ demo/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSumMapReduce(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input []int
+		want  int
+	}{
+		{"happy path", []int{1, 2, 3, 4, 5}, 15},
+		{"edge case #1", []int{}, 0},
+		{"edge case #2", []int{2, 4, 5}, 11},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := SumMapReduce(tc.input)
+			if got != tc.want {
+				t.Errorf("got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
+}
