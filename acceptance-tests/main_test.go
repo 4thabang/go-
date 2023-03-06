edffb5ad73b864496c0a23c370b32aa357a89589
4thabang <puresneakers1@gmail.com>
2023-03-06 16:56:42 +0000

    test: acceptance test

--- acceptance-tests/main_test.go
+++ acceptance-tests/main_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"go-tests/acceptance-tests/specifications"
+	"testing"
+)
+
+type some string
+
+func (s some) Greet() (string, error) {
+	if s == "" {
+		return "", fmt.Errorf("empty string")
+	}
+	return fmt.Sprintf("Hello, %s", s), nil
+}
+
+func TestGreeter(t *testing.T) {
+	s := some("")
+	specifications.GreetSpecification(t, s)
+}
