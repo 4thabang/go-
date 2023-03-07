72b1e915d239dc047f096c1bc6e495001695186b
4thabang <puresneakers1@gmail.com>
2023-03-07 12:46:15 +0000

    test(init): setup docker container test

--- acceptance-tests/cmd/httpserver/greeter_server_test.go
+++ acceptance-tests/cmd/httpserver/greeter_server_test.go
@@ -1,12 +1,40 @@
 package main_test
 
 import (
+	"context"
+	"testing"
+
 	go_specs_greet "go-tests/acceptance-tests"
 	"go-tests/acceptance-tests/specifications"
-	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreetServer(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:       "../../.",
+			Dockerfile:    "./cmd/httpserver/Dockerfile",
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
 	driver := go_specs_greet.Driver{
 		BaseURL: "http://localhost:8080",
 	}
